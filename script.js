a65cf06b77c16d803a8362cf359e49175fdc19ff
daragmas <daragmas@gmail.com>
2022-06-17 14:56:07 -0400

    Make createGameboard function, removed Play button on game start

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-// const h2 = document.createElement('h2');
-// h2.textContent = "This content added by JavaScript";
-
-// document.querySelector('body').appendChild(h2);
-
-function gameStart(){
-    console.log('test');
-}
 const penteDiv = document.createElement('div');
 penteDiv.className = 'pente';
 
+function makeGameBoard(){
+    const gameBoard = document.createElement('div');
+    gameBoard.className = 'gameBoard';
+    penteDiv.appendChild(gameBoard);
+    for (col = 0; col < 20; col++){
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        gameBoard.appendChild(cell)
+    }
+}
+
+function gameStart(){
+    penteDiv.removeChild(startButtton);
+    console.log('test');
+    makeGameBoard();
+}
+
+
+
 const startButtton = document.createElement('button');
 startButtton.textContent = "Play";
+startButtton.className = 'startButton';
 startButtton.onclick = gameStart;
 
 penteDiv.appendChild(startButtton);
