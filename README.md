ffd4e5e7094fe25c78b1147c4770f462690c499e
daragmas <daragmas@gmail.com>
2022-06-17 15:31:13 -0400

    added grid layout to game board, added note referencing game, updated To Do

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 To Do:
 - Make Pente
     + Make board
-    + Create listener for game start
+    + Create listener for game start - Done (Start Button)
     + Create listener for stone placement
         - Made confirmation of stone placement to prevent accidental places
     + Create player turns
--- script.js
+++ script.js
@@ -5,11 +5,14 @@ function makeGameBoard(){
     const gameBoard = document.createElement('div');
     gameBoard.className = 'gameBoard';
     penteDiv.appendChild(gameBoard);
-    for (col = 0; col < 20; col++){
-        let cell = document.createElement('div');
-        cell.className = 'cell';
-        gameBoard.appendChild(cell)
-    }
+    for (row = 1; row < 6; row++){
+        for (col = 1; col < 6; col++){
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.style.setProperty('grid-area', `${row} / ${col}`);
+            gameBoard.appendChild(cell)
+        }
+    }       
 }
 
 function gameStart(){
