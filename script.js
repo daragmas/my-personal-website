9ea17cc3fca9e152dddf3b532a5d372b744280d1
daragmas <daragmas@gmail.com>
2022-06-17 12:31:27 -0400

    Moved Pente Div to script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+// const h2 = document.createElement('h2');
+// h2.textContent = "This content added by JavaScript";
 
-document.querySelector('body').appendChild(h2);
+// document.querySelector('body').appendChild(h2);
+
+function gameStart(){
+    console.log('test');
+}
+const penteDiv = document.createElement('div');
+penteDiv.className = 'pente';
+
+const startButtton = document.createElement('button');
+startButtton.textContent = "Play";
+startButtton.onclick = gameStart;
+
+penteDiv.appendChild(startButtton);
+document.body.appendChild(penteDiv);
