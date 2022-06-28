d58f3f17607d8009802e530b7573823c2c3bc6ca
daragmas <daragmas@gmail.com>
2022-06-28 10:37:19 -0400

    Added click listener and player1 class addition

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const penteDiv = document.createElement('div');
 penteDiv.className = 'pente';
 
+function playGame(){
+    document.addEventListener('mousedown', function (e) {
+        // console.log(e.path[0].classList[1]);
+        let cellSelect = document.getElementsByClassName(`${e.path[0].classList[1]}`);
+        console.log(cellSelect[0].classList);
+        cellSelect[0].classList.add('player1');
+    })
+
+}
 
 function makeGameBoard(){
     const gameBoard = document.createElement('div');
@@ -9,7 +18,7 @@ function makeGameBoard(){
     for (row = 1; row < 21; row++){
         for (col = 1; col < 21; col++){
             let cell = document.createElement('div');
-            cell.className = 'cell';
+            cell.className = `cell ${row}-${col}`;
             cell.style.setProperty('grid-area', `${row} / ${col}`);
             gameBoard.appendChild(cell)
         }
@@ -20,6 +29,7 @@ function gameStart(){
     penteDiv.removeChild(startButtton);
     console.log('test');
     makeGameBoard();
+    playGame();
 }
 
 
